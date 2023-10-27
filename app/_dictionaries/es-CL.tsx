beea8a9c1a00f07f20b4ce295e8c6f71e78f20b3
Vicente Jorquera <jorquerav07@gmail.com>
2023-10-27 18:46:24 -0300

    feat/se agrega diccionario español para trabajar componentes y la UI

---
+++ app/_dictionaries/es-CL.tsx
@@ -0,0 +1,12 @@
+export const BrandInfo = {
+  title: 'Smart Parking',
+  icon: '/images/logo.webp'
+}
+
+export const CaruselInfoSignIn = [
+  {
+    imageUrl: '/images/signin.webp',
+    title: 'Administra tu estacionamiento en tiempo real desde cualquier lugar',
+    description: 'Con SmartParking puedes administrar tus plazas de estacionamiento de forma fácil y sencilla, desde tu celular, tablet o computador. Solo necesitas conexión a internet.'
+  }
+]
