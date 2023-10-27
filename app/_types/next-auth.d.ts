8ee6f82b496aec7f901f3b892259ad97e096c5f3
Vicente Jorquera <jorquerav07@gmail.com>
2023-10-27 14:57:45 -0300

    feat/se modifica interface de user para que admita roles (atributo adicional)

---
+++ app/_types/next-auth.d.ts
@@ -0,0 +1,14 @@
+import { Session } from 'next-auth'
+import { JWT } from 'next-auth/jwt'
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      name: string,
+      email: string,
+      image?: string,
+      id: string,
+      role: number
+    }
+  }
+}
