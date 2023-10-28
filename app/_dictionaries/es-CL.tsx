b83951a5272e5eadc719ed2913ff29c660e8fb89
Vicente Jorquera <jorquerav07@gmail.com>
2023-10-27 23:11:01 -0300

    feat/diccionarios y packages actualizados

--- app/_dictionaries/es-CL.tsx
+++ app/_dictionaries/es-CL.tsx
@@ -8,5 +8,20 @@ export const CaruselInfoSignIn = [
     imageUrl: '/images/signin.webp',
     title: 'Administra tu estacionamiento en tiempo real desde cualquier lugar',
     description: 'Con SmartParking puedes administrar tus plazas de estacionamiento de forma fácil y sencilla, desde tu celular, tablet o computador. Solo necesitas conexión a internet.'
+  },
+  {
+    imageUrl: '/images/signin.webp',
+    title: 'HOLA MUNDO',
+    description: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
   }
 ]
+
+export const CarouselLogIninfo = {
+  title: <h1 className='text-center text-2xl xl:text-4xl font-semibold'>INICIAR SESIÓN EN <br/><span className='text-blue-500'>SMART PARKING</span></h1>,
+  description: 'Ingresa tu correo electrónico y contraseña para acceder a tu cuenta.',
+}
+
+export const CarouselSignUpInfo = {
+  title: <h1 className='text-center text-2xl xl:text-4xl font-semibold'>REGISTRARSE EN <br/><span className='text-blue-500'>SMART PARKING</span></h1>,
+  description: 'Ingrese sus datos personales para crear su cuenta.',
+}
