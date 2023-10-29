570108c84f46b91871a59dee6d1779c31adc54c3
Vicente Jorquera <jorquerav07@gmail.com>
2023-10-29 13:46:17 -0300

    fix/se remueve footer de la pagina

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -9,7 +9,6 @@ const SignInPage = () => {
       className='font-semibold text-zinc-800 text-3xl md:text-4xl text-center'
       >{title}<br /><span className='text-blue-500'>{brand}</span></h1>
       <LoginForm />
-      <footer className='text-center absolute bottom-5 right-0 left-0 text-zinc-600'>&copy; 2023 SmartParking. Todos los derechos reservados</footer>
     </section>
   )
 }
