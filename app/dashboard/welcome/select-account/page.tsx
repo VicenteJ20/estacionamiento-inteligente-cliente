5a69f652b5766ae958dbe3263ccbb9b96f4b3c01
Vicente Jorquera <jorquerav07@gmail.com>
2023-11-01 13:00:31 -0300

    feat/se añade página de welcome

--- app/dashboard/welcome/select-account/page.tsx
+++ app/dashboard/welcome/select-account/page.tsx
@@ -1,5 +1,6 @@
 import { HeaderWelcome } from "@/app/_components/welcome/Header"
 import { WelcomeSelectAccount } from "@/app/_dictionaries/es-CL"
+import Link from "next/link"
 
 const SelectAccount = () => {
   const { title, cards, oldPage } = WelcomeSelectAccount
@@ -10,10 +11,10 @@ const SelectAccount = () => {
         {
           cards && cards.map((item, index) => (
             <li key={index}>
-              <button className='border-2 text-start border-zinc-300 rounded-md p-4 pr-12 flex flex-col gap-2 relative w-full shadow-md'>
-                <h3 className='font-semibold text-lg lg:text-xl text-zinc-700'>{item.title}</h3>
+              <Link href={item.nextPage} className={`border-2 text-start ${item.type ? 'border-blue-500' : 'border-zinc-300'} rounded-md p-4 pr-12 flex flex-col gap-2 relative w-full shadow-md hover:opacity-50 transition-all duration-300 ease-in-out`}>
+                <h3 className={`font-semibold text-lg lg:text-xl ${item.type ? 'text-blue-500': 'text-zinc-700'}`}>{item.title}</h3>
                 <p className='font-normal text-md text-zinc-500'>{item.description}</p>
-              </button>
+              </Link>
             </li>
           ))
         }
