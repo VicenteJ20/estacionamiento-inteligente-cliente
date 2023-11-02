cd18854c78e1a7842e617d7975c83492a160ca72
Vicente Jorquera <jorquerav07@gmail.com>
2023-11-01 22:12:16 -0300

    Se crea un servicio para enviar data a la api
    de crear cuenta. De momento solo es administrativa.

---
+++ app/_lib/welcome/createAccount.ts
@@ -0,0 +1,21 @@
+
+export const createAccount = async ( type: number, commercialName: string, rut: string, alias: string, address: string ) => {
+  try {
+    const response = await fetch('/api/welcome', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        type,
+        commercialName,
+        rut,
+        alias,
+        address
+      })
+    })
+    const res = await response.json()
+    return { status: response.status, message: res.message}
+  } catch (err: any) {
+    return { status: 500 }}
+}
