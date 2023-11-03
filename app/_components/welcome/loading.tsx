85415baa6293bf6d6db05ef86ee776f0ddaeb4fe
Vicente Jorquera <jorquerav07@gmail.com>
2023-11-03 00:52:58 -0300

    feat/se modifica estilo del loading

--- app/_components/welcome/loading.tsx
+++ app/_components/welcome/loading.tsx
@@ -1,8 +1,9 @@
+import { HeaderWelcome } from "./Header"
 
 export const Loading = ({ loading }: { loading: boolean }) => {
   return (
-    <div className={`bg-teal-500 absolute top-0 bottom-0 right-0 left-0 transition-all duration-300 ease-in-out ${loading ? 'opacity-100' : 'opacity-0 pointer-events-none '}`}>
-      <h2>Loading</h2>
+    <div className={`bg-white flex flex-col gap-4 items-center justify-center absolute top-0 bottom-0 right-0 left-0 transition-all duration-300 ease-in-out ${loading ? 'opacity-100' : 'opacity-0 pointer-events-none '}`}>
+      <HeaderWelcome title='Procesando...' description='Espere un momento por favor' />
     </div>
   )
 }
