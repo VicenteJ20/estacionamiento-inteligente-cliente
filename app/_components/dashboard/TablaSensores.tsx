872e1ea28c70d3ca02207985c8bd2ca8183963de
Rep235 <rnp3r3z@gmail.com>
2023-11-25 09:58:38 -0300

    feat/Se agrega la tabla funcionando con la api simulada

--- app/_components/dashboard/TablaSensores.tsx
+++ app/_components/dashboard/TablaSensores.tsx
@@ -1,80 +1,102 @@
-import React, { useState, useEffect } from "react";
-import { } from "./sensores.json"
+'use client'
+import React, {useEffect, useState} from "react";
 
+import InfiniteScroll from "react-infinite-scroll-component" //El componente es problemático, conviene cambiarlo en las siguientes iteraciones del proyecto... -R
+import {Table ,TableHeader, TableBody, TableCell,Pagination, getKeyValue, TableColumn, TableRow} from "@nextui-org/react"
+//Se anticipa la respuesta deseada de la API, en este caso se usa el tipeo del json de los sensores...  -R
+
+type TodoResponse = {
+  Id: string;
+  Status: string;
+  _ids: string;
+};
 const TablaSensores = () => {
+  // crear las constantes para los datos, el estado de carga y el estado de error   -R
+  const [data, setData] = useState([]) as any
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  
 
-    const sensores = [{"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/14/2023T171002"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/12/2023T124022"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/07/2023T003830"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/04/2023T172557"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/21/2023T213343"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/11/2023T123050"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/11/2023T142802"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/01/2023T191815"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/04/2023T104543"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/24/2023T000814"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/12/2023T184842"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/20/2023T195039"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/19/2023T091359"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/04/2023T025913"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/01/2023T200239"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/18/2023T202725"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/04/2023T123126"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/09/2023T194930"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/09/2023T103505"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/03/2023T204123"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/14/2023T183634"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/16/2023T003500"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/08/2023T005654"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/17/2023T174551"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/13/2023T144947"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/12/2023T013904"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/16/2023T005800"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/20/2023T063640"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/15/2023T063949"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/05/2023T105910"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/15/2023T145822"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/19/2023T011106"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/15/2023T081742"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/06/2023T105914"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/11/2023T112404"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/07/2023T093050"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/06/2023T064842"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/24/2023T010154"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/15/2023T091744"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/03/2023T194344"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/01/2023T174522"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/16/2023T095701"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/05/2023T220825"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/13/2023T071711"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/20/2023T093942"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/16/2023T100031"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/11/2023T095819"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/22/2023T203524"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/08/2023T024610"},
-    {"_id":"asdasdads","id":"SN001","status":"T","insertedAt":"11/19/2023T202614"}]
+// se usa useEffect para cargar los datos antes del primer render de la vista para que el navegador no  ande cagando la paciencia -R
+  useEffect(() => {
+    // Se deja el loading en true mientras se hace el fetch -R
+    setLoading(true);
 
-    return (
-        <div className="h-1/3">
-            <table >
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Estado</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {sensores.map((sensor, index) => (
-                        <tr key={index}>
-                            <td>{sensor._id}</td>
-                            <td>{sensor.id}</td>
-                            <td>{sensor.status}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+    fetch(`http://localhost:4000/data/`,{
+    method:"GET",
+    headers:{
+        contentType: "application/JSON"
+    }
+
+    })
+      .then(async (res) => {
+        // se hace el set de la data si todo sale bien... -R
+        const todo = await res.json();
+
+        setData(todo);
+
+      })
+      .catch((e) => {
+        // Se deja un error como el 400 o 404 o que se yo -R
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+      })
+      .finally(() => {
+        // dejamos la función de carga en falso al terminar el fetch -R
+        setLoading(false);
+      });
+  }, []);
+
+  // ahora se hace display del componente -R
+  const loadingComponent = <div>Cargando...</div>;
+  // display para el componente de error
+  const errorComponent = <div className="text-red-500">Error: {error}</div>;
+
+  
+
+
+console.log(data) // <----Recomiendo descomentar esto al momento de testear la conexión con la APi de los sensores   -R
+  // se hace un display acorde al useState -R
+
+  return (
+    <div className="p-24">
+      {loading ? (
+        loadingComponent
+      ) : error ? (
+        errorComponent
+      ) : (
+        <div>
+          <p>Carga completa, mostrando datos del json...</p>
+          <Table>
+          <TableHeader>
+          <TableColumn key="Id"> Id del sensor </TableColumn>
+          <TableColumn key="Status"> Status actual del sensor </TableColumn>
+          <TableColumn key="_ids"> Info </TableColumn>
+
+          </TableHeader>
+          <TableBody>
+            {
+                data && data.length > 0 && data.map((item: any, index: number) => (
+                    <TableRow key={index++}>
+                        <TableCell>
+                            {item.Id}
+                        </TableCell>
+                        <TableCell>
+                            {item.Status}
+                        </TableCell>
+                        <TableCell>
+                            {item._ids}
+                        </TableCell>
+                    </TableRow>
+                ))
+            }
+          </TableBody>
+          </Table>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default TablaSensores;
